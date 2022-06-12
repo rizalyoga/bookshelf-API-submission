70f1157febdd997fae70ab1e743f8fb5dafef5c9
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-06-12 22:43:18 +0700

    setup

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import Hapi from "@hapi/hapi";
+import routes from "./routes.js";
+
+const init = async () => {
+  const server = Hapi.server({
+    host: "localhost",
+    port: 5000,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
